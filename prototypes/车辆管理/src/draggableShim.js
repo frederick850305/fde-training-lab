79a7493e61569453b6c367c8c5f915587ac59b52
Fred <frederick850305@gmail.com>
2026-07-03 21:58:26 +0800

    clean 车辆管理

--- prototypes/车辆管理/src/draggableShim.js
+++
@@ -1,6 +0,0 @@
-export default {
-  name: 'DraggableShim',
-  props: { modelValue: { type: Array, default: () => [] } },
-  emits: ['update:modelValue', 'change'],
-  template: '<div><slot v-for="(element, index) in modelValue" name="item" :element="element" :index="index" /></div>',
-}
